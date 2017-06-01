5d8408bf4e1f63aeb1b8426e6d5d354d5f67ead1
Gray <n0220007@lm.lmig.com>
2017-06-01 16:52:12 -0400

    whatever

--- src/copyandmove/CopyAndMove.java
+++ src/copyandmove/CopyAndMove.java
@@ -1,5 +1,6 @@
 package copyandmove;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
@@ -10,41 +11,42 @@ public class CopyAndMove {
     	try {RandomAccessFile f = new RandomAccessFile(outPath, "rw");
     	f.write(bs);
     	f.close();
-    	} catch (IOException ex) { System.out.println(ex);
+    	} catch (FileNotFoundException ex) {System.out.println("the file cannot be found.");
+    	} catch (IOException ex) { System.out.println("there was an IO Exception");
     	}
     }
-    
     public static void main(String[] args) {
     	
-    	byte[] myData = new byte[] {5, 6, 7, 8, 127, 65, 5, 6, 7, 8, 127, 65, 5, 6, 7, 8, 127, 65};
-    	String outPath = "example.txt";
-    	CopyAndMove.write(myData, outPath);
-//        String filePath = "example.txt";
-//        byte[] buf = new byte[100];
-//        byte[] data = null;
-//        int dataIdx = 0;
-//        try {
-//            RandomAccessFile f = new RandomAccessFile(filePath, "r");
-//            data = new byte[(int) f.length()];
-//            while (true) {
-//                int nBytes = f.read(buf);
-//                if (nBytes == -1) {
-//                    break;
-//                }
-//                for (int i = 0; i < nBytes; i++) {
-//                    data[dataIdx] = buf[i];
-//                    dataIdx++;
-//                }
-//            }
-//            f.close();
-//        } catch (IOException ex) {
-//            System.out.println(ex);
-//            return;
-//        }
-//        Charset cs = Charset.forName("UTF-8");
-//        String s = new String(data, cs);
+    	byte[] myData = new byte[] {5, 6, 7, 8, 127, 65, 5, 6, 7, 8, 127, 65,5, 6, 7, 8, 127, 65};
+//    	String outPath = "example.txt";
+    	CopyAndMove.write(myData,"out.txt");
+        String filePath = "example.txt";
+        byte[] buf = new byte[100];
+        byte[] data = null;
+        int dataIdx = 0;
+        try {
+            RandomAccessFile f = new RandomAccessFile(filePath, "r");
+            data = new byte[(int) f.length()];
+            while (true) {
+                int nBytes = f.read(buf);
+                if (nBytes == -1) {
+                    break;
+                }
+                for (int i = 0; i < nBytes; i++) {
+                    data[dataIdx] = buf[i];
+                    dataIdx++;
+                }
+            }
+            f.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("the file to be read cannot be found");
+            return;
+        } catch (IOException ex) {
+			System.out.println("there is an IO exception.");
+		}
+        Charset cs = Charset.forName("UTF-8");
+        String s = new String(data, cs);
 //        System.out.println(s);
-//        CopyAndMove.write(data, "out.txt");
     }
 
 }
